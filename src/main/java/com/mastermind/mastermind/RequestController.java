9129d2de891147ab07fbe76e8559498a23e1efd3
harvinder52 <85876365+harvinder52@users.noreply.github.com>
2022-03-10 15:11:47 +0530

    able to return hints after some processing, need seperate methd

---
+++ src/main/java/com/mastermind/mastermind/RequestController.java
@@ -0,0 +1,56 @@
+package com.mastermind.mastermind;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class RequestController{
+	
+	@CrossOrigin("http://localhost:3000")
+	@GetMapping("/submit")
+	public String submit() {
+		
+		return "mastermind's backend is live!";
+		
+	}
+	@CrossOrigin("http://localhost:3000")
+	@PostMapping("/verifyGuess")
+    public ArrayList < Character > verifyGuess(@RequestBody int[] guess) {
+		int[] code = {2,3,4,5};
+		  ArrayList < Character > hints = new ArrayList < > ();
+	 ArrayList < Integer > list = new ArrayList < > ();
+	 
+	 for (int i = 0; i < code.length; i++) {
+
+         list.add(code[i]);
+         
+
+     }
+		for (int i = 0; i < guess.length; i++) {
+
+            if (code[i] == guess[i]) {
+                System.out.print('-');
+                hints.add('-');
+                list.set(i, 9);
+
+            }
+        }
+		 for (int i = 0; i < guess.length; i++) {
+
+             if (list.contains(guess[i]) && (code[i] != guess[i])) { //1232
+                 System.out.print("+");
+                 hints.add('+');
+                 list.set(list.indexOf(code[i]), 9);
+
+             }
+
+         }
+        return  hints;
+    }
+
+}
